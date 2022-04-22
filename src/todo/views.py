f26bf62216486c2b8b9560980590c1cc4ade1cd4
Kazuhiro <k_kishibata@yoshida-shakyo.jp>
2022-04-22 16:35:26 +0900

    add delete function

--- src/todo/views.py
+++ src/todo/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import ListView ,DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+
+from django.urls import reverse_lazy 
+
 from .models import Todo
 # Create your views here.
 class TodoList(ListView):
@@ -9,3 +12,18 @@ class TodoList(ListView):
 class TodoDetail(DetailView):
     model = Todo
     context_object_name = "task"
+
+class TodoCreate(CreateView):
+    model = Todo
+    fields = "__all__"
+    success_url = reverse_lazy("list")
+
+class TodoUpdate(UpdateView):
+    model = Todo
+    fields = "__all__"
+    sucess_url = reverse_lazy("list")
+
+class TodoDelete(DeleteView):
+    model = Todo
+    context_object_name = "task"
+    success_url = reverse_lazy("list")
